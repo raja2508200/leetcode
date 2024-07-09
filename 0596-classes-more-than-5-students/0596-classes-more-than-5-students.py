a9e54d2c56ee7ac8ac9957d7ad5b6831cdeebc91
RAJA.K <128018261+raja2508200@users.noreply.github.com>
2024-07-09 12:40:00 +0530

    Time: 566 ms (29.77%), Space: 67 MB (56.64%) - LeetHub

---
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    df=courses.groupby(["class"]).student.count().reset_index()
+    df=df.loc[df["student"]>=5,["class"]]
+    return df
+    
