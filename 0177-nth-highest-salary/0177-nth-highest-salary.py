c37c746df41ddfa3ae1a0c136577edbe8fe7d237
RAJA.K <128018261+raja2508200@users.noreply.github.com>
2024-07-18 18:59:06 +0530

    Time: 309 ms (99.83%), Space: 69.6 MB (5.78%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.py
+++ 0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -1,16 +1,9 @@
 import pandas as pd
 
-def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
-    d=set()
-    for i in employee["salary"]:
-        d.add(i)
-    c=list(set(d))
-    c.sort()
-    if(len(c)<N or len(c)==0 or N<=0 ):
-        a=None
-    else:
-        a=c[-N]
-    r="getNthHighestSalary"+"("+str(N)+")"
-    t={r:[a]}
-    o=pd.DataFrame(t)
-    return o
+def nth_highest_salary(df: pd.DataFrame, N: int) -> pd.DataFrame:
+    df = df['salary'].drop_duplicates()
+    df = df.sort_values(ascending=False)
+    column = 'getNthHighestSalary('+str(N)+')'
+    if N > len(df) or N<=0:
+        return pd.DataFrame({column:[None]})
+    return pd.DataFrame({column:[df.iloc[N-1]]})
